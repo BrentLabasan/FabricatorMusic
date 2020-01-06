0c583faf3a625ccc31777bfe623bdea932255bbb
Brent Labasan <BrentLabasan@gmail.com>
2020-01-05 16:48:55 -0800

    Updates

---
+++ precache-manifest.f0627043c17f04c7becefbe99b1c0af2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9762fafd0747e529e532e47d70da71a5",
+    "url": "/FabricatorMusic/index.html"
+  },
+  {
+    "revision": "49ddadffa5ab82efaa88",
+    "url": "/FabricatorMusic/static/css/main.c082707e.chunk.css"
+  },
+  {
+    "revision": "6373f8984e1dfba4f20a",
+    "url": "/FabricatorMusic/static/js/2.e4557c2e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FabricatorMusic/static/js/2.e4557c2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49ddadffa5ab82efaa88",
+    "url": "/FabricatorMusic/static/js/main.3b9b43e2.chunk.js"
+  },
+  {
+    "revision": "ed6b2ed3be68cc83b516",
+    "url": "/FabricatorMusic/static/js/runtime-main.cfd4b6d1.js"
+  },
+  {
+    "revision": "32ffc124c1932364161f58b9a992a6f2",
+    "url": "/FabricatorMusic/static/media/BlackPink_NWA.32ffc124.jpg"
+  },
+  {
+    "revision": "e0962a58ed7b9a9ab7a1aac05d42e158",
+    "url": "/FabricatorMusic/static/media/QNightclub.e0962a58.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FabricatorMusic/static/media/logo.5d5d9eef.svg"
+  }
+]);
